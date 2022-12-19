e1dfb6f74a27ebd6985a20e33227c0d0cbe06ecd
onur <onur.catmabacak@spherene.ch>
2022-12-19 15:46:55 +0000

    load and save mesh with pymesh is validated

--- test.py
+++ test.py
@@ -1,2 +1,14 @@
 import pymesh as pm
 
+def load_mesh(filename):
+
+    return pm.load_mesh(filename)
+
+def save_mesh(filename):
+
+    mesh = load_mesh(filename) 
+    pm.save_mesh("bunny_saved.obj", mesh)
+
+
+filename = "bunny.obj"
+save_mesh(filename)
