3ac1fd1f15072225fbb2bb2dab69734f67ddaac6
onur <onur.catmabacak@spherene.ch>
2022-12-30 13:17:58 +0000

    30.12.2022

--- test.py
+++ test.py
@@ -9,6 +9,26 @@ def save_mesh(filename):
     mesh = load_mesh(filename) 
     pm.save_mesh("bunny_saved.obj", mesh)
 
+def tetgen(filename):
+
+    mesh = load_mesh(filename) 
+    print(dir(mesh))
+    tetgen = pm.tetgen()
+    print("tetgen initiliazed")
+    tetgen.points = mesh.vertices # Input points.
+    print("tetgen initiliazed 1")
+    tetgen.triangles = mesh.faces # Input triangles
+    print("tetgen initiliazed 2")
+    tetgen.max_tet_volume = 0.01
+    print("tetgen initiliazed 3")
+    tetgen.verbosity = 0
+    print("tetgen initiliazed 4")
+    tetgen.run() # Execute tetgen
+    print("tetgen initiliazed 5")
+    mesh = tetgen.mesh
+
+    print(dir(mesh))
 
 filename = "bunny.obj"
-save_mesh(filename)
+#save_mesh(filename)
+tetgen(filename)
